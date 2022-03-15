d9203678ce4530f7f8267620dab32892935116ae
conradx <20236946+shockla@users.noreply.github.com>
2022-03-15 22:42:21 +0800

    to add drawing helper

--- main.lua
+++ main.lua
@@ -1,11 +1,10 @@
-require("helper")
-local OBJ = require("/lib/OBJ-Loader.OBJ")
+require("MTXhelper")
 
-OH = require("objHelper")
+OH = require("OBJHelper")
 
 function MTX_MVP(model2world,cameraInfo)
     local mtx_model2world = model2world--MTX_Model2World(VEC3(0,0,10),VEC3(0,0,0))
-    local mtx_world2view = MTX_World2View(CAMERA)
+    local mtx_world2view = MTX_World2View(cameraInfo)
     local mtx_view2Clip = MTX_View2Clip(cameraInfo)
 
     local mvp =  mtx_view2Clip * mtx_world2view * mtx_model2world
@@ -32,10 +31,7 @@ function love.load()
     Init()
 
     local modelPath = "testCube.obj"--"KANADE.OBJ"-- 
-    --TestCube = OBJ.loadObj(modelPath)
     MODEL = OH:loadOBJ(modelPath)
-    MODEL:SetRotation(VEC3(0,0,0))
-
 end
 
 function love.update(dt)
@@ -50,17 +46,11 @@ function love.draw()
     MODEL:SetRotation(MODEL.Rotation+VEC3(PI * dt /9,PI * dt /9,PI * dt /9))
     local mtx_MVP = MTX_MVP(MODEL.Matrix_Model2World,CAMERA)
     DrawAxis(mtx_MVP)
-    local faces = MODEL:getFaces()
-    for k,face in ipairs(faces) do
-        love.graphics.setColor(1,1,1,1)
-        DrawFace(face,mtx_MVP)
 
-        --line
-        -- love.graphics.setColor(.2,.2,.2,1)
-        -- DrawLine(face[1],face[2],mtx_MVP)
-        -- DrawLine(face[2],face[3],mtx_MVP)
-        -- DrawLine(face[3],face[4],mtx_MVP)
-        -- DrawLine(face[4],face[1],mtx_MVP)
+    local faces = MODEL:getFaces()
+    love.graphics.setColor(1,1,1,1)
+    for k,face in ipairs(faces) do
+        DrawFace(face,mtx_MVP)
     end
 end
 
@@ -86,66 +76,30 @@ function HandleInput(dt)
     end
 end
 
-function FlatTable(...)
-    local ret = {}
-    local index = 1
-    for k,v in pairs(...) do
-        for kk,vv in pairs(v) do
-            for kkk,vvv in pairs(vv) do
-                ret[index] = vvv
-            index = index + 1
-            end
-            
-        end
-    end
-
-    return ret
-end
-
 function FlatTable2(...)
     local ret = {}
     local index = 1
     for k,v in ipairs(...) do
-        --print(string.format("for(kv) k=%s, v=\n%s",k,DeepPrint(v)))
         for kk,vv in ipairs(v) do
-            --print(string.format("    for(kkvv) kk=%s, vv=\n%s",kk,DeepPrint(vv)))
             ret[index] = vv
             index = index + 1
         end
     end
-    -- print("flatTable2: "..index)
-    -- print('--ret-----\n'..DeepPrint(ret))
-    --assert(index==9,"more than 8\n"..DeepPrint(...))
-    
     return ret
 end
 
 function PrintDebugText()
     local debug_text = string.format(
-    [[FPS: %d
-Camera Info:
-%s]],
+        "FPS: %d\nCamera Info:\n%s",
         1/love.timer.getDelta(),
         DeepPrint(CAMERA,'|  '))
-    
+
         love.graphics.print(debug_text)
 end
 
---return: 
---  float: smallest Z,
---  table: vertices , need flat
 function DrawFace(model_space_face,mvp)
-    -- local verts = {}
-    -- print("==drawFace: vert count: "..#model_space_face)
-    -- for k,vertex in ipairs(model_space_face) do
-    --     verts[k] = Model2Screen(vertex,mvp)
-    -- end
-    -- verts = FlatTable2(verts)
-    -- love.graphics.polygon("line",verts)
     
     local verts,smallZ = CalcFace(model_space_face,mvp)
-    --print("smallZ = "..smallZ)
-    --print("verts:\n"..DeepPrint(verts))
     --verts = FlatTable2(love.math.triangulate(verts))
     if verts ~= nil then
         love.graphics.polygon('line',verts)
@@ -170,30 +124,15 @@ end
 --convert Model Space Coord to Screen Space
 --return: table(x,y), z
 function Model2Screen(vert,mvp)
-    --print("====Model2Screen")
-    --print(type(vert))
-    --print(DeepPrint(vert))
-    --print(DeepPrint(mvp))
     local clip_space_p = HomogeneousDivision(mvp * P(vert))
     if clip_space_p ~= nil then
-        if clip_space_p[3][1] <-1 or clip_space_p[3][1]>1 then
+        if clip_space_p[3][1] <-1.01 or clip_space_p[3][1]>1.01 then --clipping
             print("vertex clipped\n"..DeepPrint(clip_space_p))
             return
-        end --clipping
+        end 
     end
     clip_space_p = Clip2Screen * clip_space_p --screen space
     return {clip_space_p[1][1], clip_space_p[2][1]},clip_space_p[3][1]
-
-    -- local p = P(vert[1],vert[2],vert[3])
-    -- p = MTX_Model2World(VEC3(0,0,0),VEC3(0,0,0))*p
-    -- --error(DeepPrint(p))
-    -- p = MTX_World2View(CAMERA) * p
-    -- p = MTX_View2Clip(CAMERA) * p
-    -- p = HomogeneousDivision(p)
-    -- p = MTX_Clip2Screen(ScreenW,ScreenH) * p
-    -- print(string.format("x,y,z = %.3f, %.3f, %.3f",vert[1],vert[2],vert[3]))
-    -- print(string.format("x,y,z,w = %.3f, %.3f, %.3f, %.3f ",p[1][1],p[2][1],p[3][1],p[4][1]))
-    -- return {p[1][1],p[2][1]},p[3][1]
 end
 
 --model space
